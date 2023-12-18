a7e19e84fc14171af358bcf9da0aa105089e4678
PiotrDeda <piotrdeda.pl@gmail.com>
2023-12-18 13:37:54 +0100

    Added image detection

--- main.py
+++ main.py
@@ -1,26 +1,47 @@
 import os
-import sys
+
 import cv2
 import paho.mqtt.client as mqtt
 from ultralytics import YOLO
 
 
 def on_connect(client, userdata, flags, rc):
-    print(f"Connected with result code {rc}")
-    client.subscribe("test/#")
+    print(f"Connected to MQTT broker with result code {rc}")
 
 
-def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+if __name__ == '__main__':
+    print("Connecting to MQTT broker...")
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.username_pw_set(os.environ['USER'], os.environ['PASS'])
+    client.connect(os.environ['HOST'], 1883, 360)
 
+    while not client.is_connected():
+        client.loop()
 
-client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
+    camera = int(os.environ['CAMERA']) if 'CAMERA' in os.environ else 0
+    print(f"Loading camera {camera}...")
+    cap = cv2.VideoCapture(camera)
+    print("Camera loaded")
 
-client.username_pw_set(os.environ['USER'], os.environ['PASS'])
-client.connect(os.environ['HOST'], 1883, 60)
+    print("Loading YOLO model...")
+    model = YOLO('yolov8n.pt')
+    print("Model loaded")
 
-client.publish("test/sending", payload="Example", qos=0, retain=False)
+    threshold = 0.25
 
-client.loop_forever()
+    while cap.isOpened():
+        success, frame = cap.read()
+        if success:
+            results = model(frame, conf=threshold)
+            cv2.imshow("YOLO to MQTT", results[0].plot())
+            print(f"Detected {len(results[0])} results")
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        else:
+            print("Failed to read frame, shutting down...")
+            break
+
+    client.disconnect()
+    cap.release()
+    cv2.destroyAllWindows()
