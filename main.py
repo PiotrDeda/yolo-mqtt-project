708d8c7a0107f969bec473aabf08a0918600efb7
PiotrDeda <piotrdeda.pl@gmail.com>
2024-01-14 21:45:10 +0100

    Added message sending

--- main.py
+++ main.py
@@ -29,13 +29,20 @@ if __name__ == '__main__':
     print("Model loaded")
 
     threshold = 0.25
+    prev_people_count = 0
 
     while cap.isOpened():
         success, frame = cap.read()
         if success:
             results = model(frame, conf=threshold)
+            people_count = sum([1 for label in results[0].names if label == 'person'])
+            print(f"Detected {people_count} results")
+
+            if people_count != prev_people_count:
+                client.publish("people_count", str(people_count))
+                prev_people_count = people_count
+
             cv2.imshow("YOLO to MQTT", results[0].plot())
-            print(f"Detected {len(results[0])} results")
             if cv2.waitKey(1) & 0xFF == ord("q"):
                 break
         else:
