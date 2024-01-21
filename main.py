e12d8ba82c2b654c21c0bf14679933ca465a5947
PiotrDeda <piotrdeda.pl@gmail.com>
2024-01-21 22:02:12 +0100

    General improvements

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import configparser
+import os
 import time
 
 import cv2
@@ -11,38 +12,48 @@ def on_connect(client, userdata, flags, rc):
 
 
 if __name__ == '__main__':
+    # Load config
     config = configparser.ConfigParser()
     config.read('config.ini')
-    slowdown = not config['YOLO'].getboolean('RapidMode')
+    username = os.environ['MQTT_USERNAME'] if 'MQTT_USERNAME' in os.environ else config['MQTT']['Username']
+    password = os.environ['MQTT_PASSWORD'] if 'MQTT_PASSWORD' in os.environ else config['MQTT']['Password']
+    host = os.environ['MQTT_HOST'] if 'MQTT_HOST' in os.environ else config['MQTT']['Host']
+    port = int(os.environ['MQTT_PORT']) if 'MQTT_PORT' in os.environ else config['MQTT'].getint('Port')
+    camera = config['YOLO'].getint('Camera')
+    rapid_mode = config['YOLO'].getboolean('RapidMode')
+    preview = config['YOLO'].getboolean('Preview')
+    threshold = config['YOLO'].getfloat('Threshold')
 
+    # Connect to MQTT broker
     print("Connecting to MQTT broker...")
     client = mqtt.Client()
     client.on_connect = on_connect
-    client.username_pw_set(config['MQTT']['Username'], config['MQTT']['Password'])
-    client.connect(config['MQTT']['Host'], 1883, 360)
+    client.username_pw_set(username, password)
+    client.connect(host, port, 360)
 
     while not client.is_connected():
         client.loop()
 
-    camera = int(config['YOLO']['Camera']) if 'CAMERA' in config['YOLO'] else 0
+    # Load camera
     print(f"Loading camera {camera}...")
     cap = cv2.VideoCapture(camera)
     print("Camera loaded")
 
+    # Load YOLO
     print("Loading YOLO model...")
     model = YOLO('yolov8n.pt')
     print("Model loaded")
 
-    threshold = 0.25
+    # Main detection loop
     prev_people_count = 0
-
     while cap.isOpened():
         success, frame = cap.read()
         if success:
             results = model(frame, conf=threshold)
-            cv2.imshow("YOLO to MQTT", results[0].plot())
-            if cv2.waitKey(1) & 0xFF == ord("q"):
-                break
+            if preview:
+                cv2.imshow("YOLO to MQTT", results[0].plot())
+                if cv2.waitKey(1) & 0xFF == ord("q"):
+                    break
             label_id = (list(results[0].names))[list(results[0].names.values()).index('person')]
             people_count = sum([1 for i in results[0].boxes.cls if i == label_id])
             print(f"Detected {people_count} results")
@@ -51,12 +62,13 @@ if __name__ == '__main__':
                 client.publish("yolo/people_count", str(people_count))
                 prev_people_count = people_count
 
-            if prev_people_count > 0 and slowdown:
+            if prev_people_count > 0 and not rapid_mode:
                 time.sleep(2)
         else:
             print("Failed to read frame, shutting down...")
             break
 
+    # Cleanup
     client.disconnect()
     cap.release()
     cv2.destroyAllWindows()
